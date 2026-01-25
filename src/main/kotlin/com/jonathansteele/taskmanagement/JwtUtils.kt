1e089f7e3ffe3d9ff4877dfb371fc30745498021
Jonathan Steele <iNoles@users.noreply.github.com>
2026-01-25 14:30:09 -0500

    Refactor JwtUtils.kt for concise function definitions

--- src/main/kotlin/com/jonathansteele/taskmanagement/JwtUtils.kt
+++ src/main/kotlin/com/jonathansteele/taskmanagement/JwtUtils.kt
@@ -32,8 +32,7 @@ class JwtUtils {
     private val jwtCookie = "JWT"
     private val refreshCookieName = "refresh_token"
 
-    fun getJwtFromCookies(request: HttpServletRequest): String? =
-        WebUtils.getCookie(request, jwtCookie)?.value
+    fun getJwtFromCookies(request: HttpServletRequest): String? = WebUtils.getCookie(request, jwtCookie)?.value
 
     fun generateJwtCookie(username: String): ResponseCookie {
         val jwt = generateJwtToken(username)
@@ -47,11 +46,9 @@ class JwtUtils {
         return generateOrClearCookie(refreshToken, maxAge)
     }
 
-    fun clearJwtCookie(): ResponseCookie =
-        generateOrClearCookie(jwtCookie, 0)
+    fun clearJwtCookie(): ResponseCookie = generateOrClearCookie(jwtCookie, 0)
 
-    fun clearRefreshCookie(): ResponseCookie =
-        generateOrClearCookie(refreshCookieName, 0)
+    fun clearRefreshCookie(): ResponseCookie = generateOrClearCookie(refreshCookieName, 0)
 
     private fun generateOrClearCookie(cookieName: String, maxAge: Long): ResponseCookie =
         ResponseCookie
@@ -61,11 +58,9 @@ class JwtUtils {
             .maxAge(maxAge)
             .build()
 
-    fun generateJwtToken(username: String): String =
-        generateToken(username, jwtExpirationMs)
+    fun generateJwtToken(username: String): String = generateToken(username, jwtExpirationMs)
 
-    fun generateRefreshToken(username: String): String =
-        generateToken(username, refreshExpirationMs)
+    fun generateRefreshToken(username: String): String = generateToken(username, refreshExpirationMs)
 
     fun getUsernameFromToken(token: String): String =
         Jwts
